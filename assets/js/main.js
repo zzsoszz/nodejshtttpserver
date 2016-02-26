886903e08578c36cb673525f8851520e05ee874d
137573155@qq.com <137573155@qq.com>
2016-02-26 17:06:56 +0800

    init

--- assets/js/main.js
+++
@@ -1,9 +0,0 @@
-$(document).ready(function(){
-	
-	$(".file").change(function(){
-		var aa=$(this).get(0).files[0];
-		var uploadsrc = window.URL.createObjectURL(aa);
-		$(this).closest(".file-box").find("img")[0].src=uploadsrc;
-	});
-	
-});
