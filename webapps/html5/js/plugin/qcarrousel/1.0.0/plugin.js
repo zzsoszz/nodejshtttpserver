fd35f189bc6e0bdf44b6e3e6bb737252a035fec3
137573155@qq.com <137573155@qq.com>
2016-06-24 16:09:52 +0800

    update

---
+++ webapps/html5/js/plugin/qcarrousel/1.0.0/plugin.js
@@ -0,0 +1,55 @@
+(function($){
+
+		var plugname="qcarrousel";
+		$.fn[plugname]=function()
+		{
+			var isMethodCall=arguments.length>0 && typeof arguments[0] === "string";
+			if(isMethodCall)
+			{
+				
+				var methodname=arguments[0];
+				var args = Array.prototype.slice.call(arguments,1);
+				this.each(function() {
+					var instance = $.data( this,plugname);
+					if(instance && $.isFunction( instance[methodname] ))
+					{
+						var method=instance[methodname];
+						method.apply(instance,args);
+					}
+				});
+			}else{
+				var inputoptions = arguments;
+				$(this).each(
+						function ()
+						{
+							var optionsnew = $.extend( {}, defaultoptions);
+							if(inputoptions.length>0)
+							{
+									optionsnew=$.extend(optionsnew,inputoptions[0]);
+							}
+							var instance=$(this).data(plugname);
+							if(instance)
+							{
+								instance.init(optionsnew);
+							}else
+							{
+								var target=$(this);
+								instance=new PluginObject(target);
+								instance.init(optionsnew);
+								$(this).data(plugname,instance);
+							}
+						}
+					);
+					return this;
+			};
+		}
+
+		function PluginObject(target)
+		{
+			this.init=function()
+			{
+				
+			}
+		}
+
+})(jQuery);
