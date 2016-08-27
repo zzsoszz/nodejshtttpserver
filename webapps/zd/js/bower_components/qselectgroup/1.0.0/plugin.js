82cd98b6bb197159bcd4f24f6a082b9bf9ab0466
zzsoszz <137573155@qq.com>
2016-08-27 11:42:10 +0800

    update

--- webapps/zd/js/bower_components/qselectgroup/1.0.0/plugin.js
+++ webapps/zd/js/bower_components/qselectgroup/1.0.0/plugin.js
@@ -2,23 +2,42 @@ if(angular && angular.module)
 {
 	
 	var qui=angular.module("qui",[]);
-	//angular.element("body>div.ng-scope").scope().username
-	qui.directive('qgroupselect',[function() {
+	qui.directive('qgroupselect',['$templateRequest','$compile',function($templateRequest,$compile) {
 	        return {
 	            restrict: 'A',
 	            priority: 1,
-	            templateUrl: 'plugin.html',
 	            require: '?ngModel',
-	            scope:{},
-	            replace: false,
-	            transclude:false,
-	            controller:function()
-	            {
-
-	            },
-	            link: function(scope, element, attrs,controller) {
-
-	            }
+	            controller: function($scope,$element) {
+				  $scope.userlist = [{
+					username: 'qingtian',
+					password: '123456'
+				  }];
+				  $scope.selectedItem;
+				  $scope.add=function(item){
+					  $scope.userlist.push({
+						username: 'user'+$.guid,
+						password: '144'
+					  });
+				  };
+				  $scope.addToSelected=function(item){
+					  console.log("item",item);
+					  $scope.selectedItem=item;
+					  $element.controller("ngModel").$setViewValue(item.username);
+					  $element.controller("ngModel").$render();
+				  };
+				},
+				link: function(scope, element, attrs) {
+				  
+				  $templateRequest("plugin.html", false).then(
+					function(viewFn) {
+					  
+					  var v = angular.element(viewFn);
+					  angular.element("body").append(v);
+					  $compile(v)(scope);
+					}
+				  );
+				  
+				}
 	        };
 	    }]
     );
