f0c431b2a2c534d6b77b9de37632efdbaec60422
hyq2015 <18030638805@163.com>
2016-09-13 11:34:27 +0800

    通过服务方式获取数据

---
+++ webapps/zd/js/bower_components/qselectgroup/1.0.2/plugin.js
@@ -0,0 +1,123 @@
+if(angular && angular.module)
+{
+	var qui;
+	try{
+		qui=angular.module("qui")
+	}catch(e)
+	{
+		qui=angular.module("qui",[]);
+	}
+	qui.directive('qgroupselect',['$templateRequest','$compile','qcityservice',function($templateRequest,$compile,qcityservice) {
+	        return {
+	            restrict: 'A',
+	            priority: 1,
+	            scope:true,
+	            require: '?ngModel',
+	            controller: function($scope,$element,$attrs) {
+	              $scope.qgroupselectLayerEle;
+				  $scope.reponsedata=[];
+				  $scope.data={
+				  	isVisible:true,
+				  	keyWords:"",
+				  	startTime:"",
+				  	typeId:1,
+				  	pageNo:1,
+				  	pageSize:5
+				  };
+
+				  $scope.qpageroptions={
+			        totalpage:0,
+			        currentpage:1
+			      };
+
+			      $scope.$watch("qpageroptions.currentpage",function(newval,oldval)
+				  {
+					 console.log("pagechange:",newval);
+					 $scope.data.pageNo=newval;
+					 if($scope.qpageroptions.totalpage!=0)
+					 {
+					 	$scope.search();
+					 }
+				  });
+
+				  $scope.addToSelected=function(item){
+					  console.log("item",item);
+					  $scope.selectedItem=item;
+					  //$element.controller("ngModel").$setViewValue(item.id);
+					  //$element.controller("ngModel").$render();
+					  //$scope.
+					  $scope[$attrs["ngModel"]]=$scope.selectedItem;
+					  console.log("$scope",$scope[$attrs["ngModel"]]);
+				  };
+				  
+				  $scope.search=function(form)
+			      {
+			      		console.log($scope.qgroupselectLayerEle);
+				      	var params=$.param($scope.data);
+				      	console.log("params:",params);
+				      	qcityservice.get().success(function(data) {
+					     		$scope.reponsedata=data.json.list;
+					     		$scope.qpageroptions.totalpage=Math.ceil(data.json.count/$scope.data.pageSize);
+				      	});
+				 		//     	$.ajax({
+				  		//     	 	 type: "POST",
+						//      url: "http://localhost:3000/zd/js/bower_components/qselectgroup/1.0.1/test.json",
+						//      data:params,
+						//      crossDomain:true,
+						//      dataType: 'json',
+						//      success: function(data) 
+						//      {
+						//      	$scope.$apply(function()
+						//      	{
+						//      		$scope.reponsedata=data.json.list;
+						//      		$scope.qpageroptions.totalpage=Math.ceil(data.json.count/$scope.data.pageSize);
+						//      		//console.log($scope.reponsedata);
+						//      	});
+						//      }
+						// });
+			       };
+				},
+				link: function(scope, element, attrs) {
+
+				  $templateRequest("plugin.html", false).then(
+					function(viewFn) {
+
+					  var v = angular.element(viewFn).attr("id","qgroupselectLayerEle_"+$.guid).hide();
+					  scope.qgroupselectLayerEle=v;
+					  scope.ele=element;
+					  scope.ele.on("click",$.proxy(function(event){
+							event.preventDefault();
+							this.qgroupselectLayerEle.show();
+					  },scope));
+					  var myfn=$.proxy(function(event){
+					  		console.log(event.target,$(event.target).closest(".qselectgroup"));
+							if(event.target!=this.ele.get(0) && $(event.target).closest(".qselectgroup").length  < 1 && $(event.target).closest(".qselectgrouptarget").length  < 1) 
+							{
+								  this.qgroupselectLayerEle.hide();
+							}
+					  },scope);
+					  $(document).on("click",myfn);
+					  scope.$on('$destroy', function() {
+								console.log(element,"------------destroying");
+								$(document).off("click",myfn);
+					  });
+					  angular.element("body").append(v);
+					  $compile(v)(scope);
+
+					}
+				  );
+
+				  element.on('$destroy', function() {
+				  	console("  destroying");
+		          });
+				  
+				}
+	        };
+	    }]
+    );
+
+}
+
+
+
+
