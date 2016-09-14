b768788c19d612d1aac65584e9a09b14f09927fe
hyq2015 <18030638805@163.com>
2016-09-14 13:15:45 +0800

    update

---
+++ webapps/zd/js/bower_components/qimgpreview/1.0.0/plugin.js
@@ -0,0 +1,51 @@
+
+if(angular && angular.module)
+{
+
+    function getFileUrl(sourceId) {
+        var url;
+        if (navigator.userAgent.indexOf("MSIE") >= 1) { // IE 
+            url = document.getElementById(sourceId).value;
+        } else if (navigator.userAgent.indexOf("Firefox") > 0) { // Firefox 
+            url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));
+        } else if (navigator.userAgent.indexOf("Chrome") > 0) { // Chrome 
+            url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));
+        }
+        return url;
+    }
+    function preImg(sourceId, targetId) {
+        var url = getFileUrl(sourceId);
+        var imgPre = document.getElementById(targetId);
+        imgPre.src = url;
+    }
+	var qui;
+	try{
+		qui=angular.module("qui")
+	}catch(e)
+	{
+		qui=angular.module("qui",[]);
+	}
+	qui.directive('qimgpreview',['$templateRequest','$compile',function($templateRequest,$compile) {
+	    return {
+		        restrict: 'A',
+		        priority: 100,
+		        require: '?ngModel',
+		        scope: {
+		            items:'=dropdowndata',
+		            initdata:'=initdata',
+		            ngModel: '=ngModel'
+		        },
+		        link: function(scope, element, attrs,controller) {
+		        	var fileinput=$('<input type="file">');
+		        	element.append();
+		        }
+		    };
+		}]
+	);
+
+	
+}
+
+
+
+
