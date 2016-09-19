a97fd79819b396cddfb9eb699819d9a700785031
hyq2015 <18030638805@163.com>
2016-09-19 14:51:43 +0800

    update

--- webapps/zd/js/bower_components/qdatatable/1.0.1/plugin.js
+++ webapps/zd/js/bower_components/qdatatable/1.0.1/plugin.js
@@ -7,17 +7,45 @@ if(angular && angular.module)
 	{
 		qui=angular.module("qui",[]);
 	}
+	qui.directive('ngRepeatRange', ['$compile', function ($compile) {
+	    return {
+	        replace: true,
+	        scope: { from: '=', to: '=', step: '=' },
+
+	        link: function (scope, element, attrs) {
+	            // returns an array with the range of numbers
+	            // you can use _.range instead if you use underscore
+	            function range(from, to, step) {
+	                var array = [];
+	                while (from + step <= to)
+	                    array[array.length] = from += step;
+
+	                return array;
+	            }
+	            // prepare range options
+	            var from = scope.from || 0;
+	            var step = scope.step || 1;
+	            var to   = scope.to || attrs.ngRepeatRange;
+	            // get range of numbers, convert to the string and add ng-repeat
+	            var rangeString = range(from, to + 1, step).join(',');
+	            angular.element(element).attr('ng-repeat', 'n in [' + rangeString + ']');
+	            angular.element(element).removeAttr('ng-repeat-range');
+	            $compile(element)(scope);
+	        }
+	    };
+	}]);
 	qui.component('qdatatable', {
 	  transclude: true,
 	  bindings:{
 	    items: '=ngModel',
 	    option: '=ngOption'
 	  },
+	  scope:true,
 	  templateUrl:baseUrl+'js/bower_components/qdatatable/1.0.1/plugin.html',
 	  controller: function() {
 	  	this.$onInit=function()
 	  	{
-	  		this.items=[{"createtime":"09/05/2016 - 09/13/2016","school":"天津1","major":"河北区1"},{"createtime":"09/05/2016 - 09/13/2016","school":"天津2","major":"河北区2"},{"createtime":"09/05/2016 - 09/13/2016","school":"天津3","major":"河北区3"}];
+	  		this.items=[{"createtime":"09/05/2016 - 09/13/2016","school":"天津3","major":"河北区3","province":"天津","desc":"aaa","lessons":"qingtian"}];
 	  		this.schoolprovinces =provinceCitys.map(function(obj1){
 		            obj1.sub=obj1.sub.map(function(obj){
 		               return {id:obj.name,name:obj.name};
@@ -28,6 +56,7 @@ if(angular && angular.module)
 	  	this.showAdd=function()
 	  	{
 	  		this.isShowPanel=!this.isShowPanel;
+	  		this.item={};
 	  	};
 	  	this.doAdd=function($event,item)
 	  	{
