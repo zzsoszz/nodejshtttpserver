ec92500f80ca8784200aff6438efc99453113862
hyq2015 <18030638805@163.com>
2016-09-26 18:11:05 +0800

    update

--- webapps/zd/js/bower_components/qimgpreview/1.0.0/plugin.js
+++ webapps/zd/js/bower_components/qimgpreview/1.0.0/plugin.js
@@ -2,20 +2,20 @@
 if(angular && angular.module)
 {
 
-    function getFileUrl(sourceId) {
+    function getFileUrl(ele) {
         var url;
         if (navigator.userAgent.indexOf("MSIE") >= 1) { // IE 
-            url = document.getElementById(sourceId).value;
+            url = ele.value;
         } else if (navigator.userAgent.indexOf("Firefox") > 0) { // Firefox 
-            url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));
+            url = window.URL.createObjectURL(ele.files.item(0));
         } else if (navigator.userAgent.indexOf("Chrome") > 0) { // Chrome 
-            url = window.URL.createObjectURL(document.getElementById(sourceId).files.item(0));
+            url = window.URL.createObjectURL(ele.files.item(0));
         }
         return url;
     }
-    function preImg(sourceId, targetId) {
-        var url = getFileUrl(sourceId);
-        var imgPre = document.getElementById(targetId);
+    function preImg(fileEle, imgEle) {
+        var url = getFileUrl(fileEle);
+        var imgPre = imgEle;
         imgPre.src = url;
     }
 	var qui;
@@ -39,7 +39,7 @@ if(angular && angular.module)
 		        	var fileinput=$('<input type="file">');
 		        	var img=$("<img>");
 		        	element.append(fileinput).click(function(){
-		        		fileimput
+		        		fileimput.
 		        	});
 		        }
 		    };
