1f6ebb2c7c4cfa35b2288b44bd97ca887f2a4a75
hyq2015 <18030638805@163.com>
2016-09-26 15:42:46 +0800

    add

--- webapps/zd/js/app/qmicrocourse/controller.js
+++ webapps/zd/js/app/qmicrocourse/controller.js
@@ -266,13 +266,67 @@ qmicrocourse.controller("editController", function ($rootScope,$http,$location,$
 });
 
 qmicrocourse.controller("addController", function ($rootScope,$http,$location,$scope,$stateParams) {
-  $scope.username="add-"+$stateParams.id;
-  $scope.$on("$destroy",function(){
-    console.log("add-destroy",$scope);
-  })
-  $scope.$on("$init",function(){
-    console.log("add-init",$scope);
-  })
+  
+   $scope.$on("$destroy",function(){
+      console.log("add-destroy",$scope);
+   })
+   $scope.$on("$init",function(){
+      console.log("add-init",$scope);
+   })
+   $scope.daterangeoption={
+        maxDate:new Date(new Date().getFullYear()+'-'+(new Date().getMonth()+1)+'-'+(new Date().getDate()+1)),
+        alwaysShowCalendars:true,
+        autoApply:true,
+        "ranges": {
+            '不限时间': ["1900-01-01","2050-01-01"],
+            '今天': [moment(), moment()],
+            '昨天': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            '这周': [moment().startOf('week'), moment()],
+            '最近7天': [moment().subtract(6, 'days'), moment()],
+            '最近30天': [moment().subtract(29, 'days'), moment()],
+            '当月': [moment().startOf('month'), moment().endOf('month')],
+            '上个月': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+        },
+        locale : {
+            format : 'YYYY-MM-DD',
+            applyLabel : '确定',
+            cancelLabel : '取消',
+            fromLabel : '起始时间',
+            toLabel : '结束时间',
+            customRangeLabel : '自定义',
+            daysOfWeek : [ '日', '一', '二', '三', '四', '五', '六' ],
+            monthNames : [ '一月', '二月', '三月', '四月', '五月', '六月',
+                '七月', '八月', '九月', '十月', '十一月', '十二月' ],
+            firstDay : 1
+        },
+        autoUpdateInput:true,
+        singleDatePicker:false,
+        startDate:'1980-01-01',
+        endDate:'2050-01-01',
+        showDropdowns : true
+    };
+    var schoolprovinces =provinceCitys.map(function(obj1){
+                obj1.sub=obj1.sub.map(function(obj){
+                   return {id:obj.name,name:obj.name};
+                });
+                return {id:obj1.name,name:obj1.name,sub:obj1.sub};
+    });
+    var lessonArray =[{id:1,name:"qingtian"},{id:2,name:"qingtian1"}];
+    var genderArray =[{id:'1',name:"男"},{id:'2',name:"女"}];
+
+    
+    $scope.model=[
+       {title:'编号',name:"id",type:"text"},
+       {title:'名称',name:"name",type:"text"},
+       {title:'时间',name:"createtime",type:"daterangepicker"},
+       {title:'省份',name:"province",type:"muliplyselect",typedata:schoolprovinces},
+       {title:'学校',name:"school",type:"muliplyselect",parent:"province"},
+       {title:'编号',name:"id",type:"text",require:true},
+       {title:'文字',name:"name",type:"text",require:true},
+       {title:'多选',name:"lessons",type:"muliplyselect",typedata:lessonArray},
+       {title:'html格式',name:"actions",type:"html"},
+    ];
+    
 });
 
 
