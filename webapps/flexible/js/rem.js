df098223f1a56f3a63495015bbb3f6801359e69a
hyq2015 <18030638805@163.com>
2017-01-04 16:56:31 +0800

    update

---
+++ webapps/flexible/js/rem.js
@@ -0,0 +1,54 @@
+function getDpr()
+{
+	var  dpr = window.devicePixelRatio || 1; 
+	console.log("dpr",dpr);
+	return dpr;
+}
+/*
+  取得缩放后的rem
+*/
+function getHtmlFontSize()
+{
+	var designWidth=640;
+	var deviceWidth=document.documentElement.getBoundingClientRect().width;
+	var deviceWidth2=document.documentElement.clientWidth;//window.innerWidth;
+	var deviceWidth3=window.innerWidth;
+	console.log("deviceWidth",deviceWidth);
+	console.log("deviceWidth2",deviceWidth2);
+	console.log("deviceWidth3",deviceWidth3);
+	if(window.innerWidth>designWidth){
+			//deviceWidth=designWidth;
+	}
+	var  htmlFontSize=100 * (deviceWidth/ designWidth) * getDpr()+ 'px';
+	return htmlFontSize;
+}
+
+
+
+(function (doc, win) {
+	var docEl = doc.documentElement;
+	var  scale = parseFloat((1 / getDpr()).toFixed(2)); 
+	//scale = parseFloat((1 / dpr).toFixed(2));  
+	console.log("scale:",scale);
+	var metaEl;
+	if (!metaEl) {
+        metaEl = doc.createElement('meta');
+        metaEl.setAttribute('name', 'viewport');
+        metaEl.setAttribute('content', 'initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=yes');
+        doc.getElementsByTagName("head")[0].appendChild(metaEl);
+	 }
+})(document, window);
+
+
+
+(function (doc, win) {
+	var docEl = doc.documentElement,
+		resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
+		recalc = function () {
+			docEl.style.fontSize=getHtmlFontSize();
+		};
+	if (!doc.addEventListener) return;
+	win.addEventListener(resizeEvt, recalc, false);
+	doc.addEventListener('DOMContentLoaded', recalc, false);
+})(document, window);
+
