047af536d7131306bf9f00163ebf08c57f5a7c00
zzsoszz <137573155@qq.com>
2017-01-05 00:06:47 +0800

    update

--- webapps/flexible/js/rem.js
+++ webapps/flexible/js/rem.js
@@ -5,19 +5,22 @@ function getDpr()
 	return dpr;
 }
 
-
 /*
   取得缩放后的rem
+  	// var deviceWidth=document.documentElement.getBoundingClientRect().width;
+	// var deviceWidth2=document.documentElement.clientWidth;//window.innerWidth;
+	// var deviceWidth3=window.innerWidth;
+	// console.log("deviceWidth",deviceWidth);
+	// console.log("deviceWidth2",deviceWidth2);
+	// console.log("deviceWidth3",deviceWidth3);
+	// if(window.innerWidth>designWidth){
+	// 		//deviceWidth=designWidth;
+	// }
 */
 function getHtmlFontSize()
 {
 	var designWidth=640;
-	var deviceWidth=document.documentElement.getBoundingClientRect().width;
-	var deviceWidth2=document.documentElement.clientWidth;//window.innerWidth;
-	var deviceWidth3=window.innerWidth;
-	console.log("deviceWidth",deviceWidth);
-	console.log("deviceWidth2",deviceWidth2);
-	console.log("deviceWidth3",deviceWidth3);
+	var deviceWidth=screen.width;
 	if(window.innerWidth>designWidth){
 			//deviceWidth=designWidth;
 	}
@@ -27,19 +30,19 @@ function getHtmlFontSize()
 
 
 
-// (function (doc, win) {
-// 	var docEl = doc.documentElement;
-// 	var  scale = parseFloat((1 / getDpr()).toFixed(2)); 
-// 	//scale = parseFloat((1 / dpr).toFixed(2));  
-// 	console.log("scale:",scale);
-// 	var metaEl;
-// 	if (!metaEl) {
-//         metaEl = doc.createElement('meta');
-//         metaEl.setAttribute('name', 'viewport');
-//         metaEl.setAttribute('content', 'width=device-width,initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=yes');
-//         doc.getElementsByTagName("head")[0].appendChild(metaEl);
-// 	 }
-// })(document, window);
+(function (doc, win) {
+	var docEl = doc.documentElement;
+	var  scale = parseFloat((1 / getDpr()).toFixed(2)); 
+	//scale = parseFloat((1 / dpr).toFixed(2));  
+	console.log("scale:",scale);
+	var metaEl;
+	if (!metaEl) {
+        metaEl = doc.createElement('meta');
+        metaEl.setAttribute('name', 'viewport');
+        metaEl.setAttribute('content', 'width=device-width,initial-scale=' + scale + ', maximum-scale=' + scale + ', minimum-scale=' + scale + ', user-scalable=yes');
+        doc.getElementsByTagName("head")[0].appendChild(metaEl);
+	 }
+})(document, window);
 
 
 
